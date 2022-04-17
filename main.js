8cddf5c46eb43fe1c90bac14d34fa984af36a81d
srikaransai <98792524+srikaransai@users.noreply.github.com>
2022-04-17 15:44:50 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg", "https://i.ytimg.com/vi/CJvgi4N4UDs/maxresdefault.jpg"];
+var names = ["Family Book","Grandpa steve", "srika", "raman", "mom", "dad", "dababy"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 6
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = images[i];
+    
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = names[i] ;
+    
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+
+}
